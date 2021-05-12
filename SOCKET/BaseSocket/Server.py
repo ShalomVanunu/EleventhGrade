513eaf0682ab9b2ecbff963312617043e420c9f8
ShalomVanunu <67192981+ShalomVanunu@users.noreply.github.com>
2021-05-12 14:21:33 +0300

    Updated 12/5/2021

---
+++ SOCKET/BaseSocket/Server.py
@@ -0,0 +1,28 @@
+import socket
+
+SERVERIP = "192.168.1.159" #The IP of Server
+PORT = 5050 # well known PORTs  (1-1023)
+
+#Phase 1 - Create socket Object
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)   #socket.AF_INET = IPv4   || SOCK_STREAM -TCP SOCK_DGRAM-UDP
+
+# Phase 2 - Bind the Object to MY IP,and PORT
+server.bind((SERVERIP,PORT))  #Tupple
+
+print(" The Server is UP")
+
+# Phase 3 - getiing the Server to Listen
+server.listen()
+
+# Phase 4 - wait for Client to connect and get his adress and Conn Obect
+conn, address = server.accept()
+
+print(f" The Client {address} is connected")
+
+#Phase 5 - get the data from client. no more than 500 bytes. decode the data in utf-8
+msg = conn.recv(500).decode('utf-8')
+
+print(msg)
+
+#Phase 6 - Close the Connection
+conn.close()
